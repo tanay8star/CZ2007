4b22bbd956a0b05c587ac4f7ef038d6cc534073b
Tanay Bharadwaja <45331632+tanay8star@users.noreply.github.com>
2020-06-18 12:57:01 +0800

    Filtered and cleaned

--- EC3_ SQL/ARCHIVE/TEST.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Timetable VALUES ('Saturday', '10:00:00', '11:00:00', 'LT1A', 'Lecture')
-INSERT INTO Timetable VALUES ('Tuesday', '10:00:00', '11:00:00', 'LT1A', 'Lecture')
--- EC3_ SQL/Archivesssssssssssssssssssssssssssssss/ARCHIVES AGAIN AGAIN/TEST.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-FROM Student, Person_R1
-WHERE Student.personID = Person_R1.personID
-AND Person_R1.school = 'Nanyang Business School';
